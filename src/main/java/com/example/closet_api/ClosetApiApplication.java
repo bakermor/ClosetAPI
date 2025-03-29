4b6103ea0c97ed203c15073d20b8ea4b7f1c8a86
Helen Schawe <helen.schawe@gmail.com>
2025-03-29 15:17:19 -0500

    MongoDB using Spring framework

--- src/main/java/com/example/closet_api/ClosetApiApplication.java
+++ src/main/java/com/example/closet_api/ClosetApiApplication.java
@@ -2,10 +2,15 @@ package com.example.closet_api;
 
 // Import Java modules
 import java.lang.System;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-// Import external dependencies (Spring)
-import org.bson.BsonDocument;
-import org.bson.Document;
+// Import external dependencies
+// Spring
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
@@ -14,6 +19,10 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+// MongoDB imports
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.bson.BsonDocument;
+import org.bson.Document;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
@@ -30,29 +39,32 @@ import com.mongodb.client.model.Updates;
 import com.mongodb.client.result.DeleteResult;
 import com.mongodb.client.result.InsertManyResult;
 import org.bson.codecs.configuration.CodecRegistry;
-//import org.bson.codecs.pojo.PojoCodecProvider;
 import org.bson.conversions.Bson;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
-
 // Import our storage service
 import com.example.closet_api.storage.StorageProperties;
 import com.example.closet_api.storage.StorageService;
+import com.example.closet_api.database.model.UserItem;
+import com.example.closet_api.database.model.ClothesItem;
+import com.example.closet_api.database.repository.UserItemRepository;
+//import com.example.database.repository.CustomItemRepository;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
 @ComponentScan({"com.example.closet_api.basic", "com.example.closet_api.storage"})
+@EnableMongoRepositories
 public class ClosetApiApplication {
 
     private static ApplicationContext applicationContext;
+
+    @Autowired      // ItemRepository is autowired, allowing Spring to find it automatically.
+    UserItemRepository userItemRepo;
+
+//    @Autowired
+//    CustomItemRepository customRepo;
 
     @Bean
     CommandLineRunner init(StorageService storageService) {
@@ -64,26 +76,6 @@ public class ClosetApiApplication {
     }
 
     public static void main(String[] args) {
-        // Set system properties via commandline or programmatically
-        System.setProperty("javax.net.ssl.keyStore", "./cert/closet-api-app-1_x509.pem");
-        //System.setProperty("javax.net.ssl.keyStorePassword", "<keystore_password>");
-
-        String uri = "mongodb+srv://closetdb.o2kklsl.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&appName=ClosetDB";
-        ConnectionString connectionString = new ConnectionString(uri);
-        MongoClientSettings settings = MongoClientSettings.builder()
-                .applyConnectionString(connectionString)
-                .serverApi(ServerApi.builder()
-                        .version(ServerApiVersion.V1)
-                        .build())
-                .build();
-        MongoClient mongoClient = MongoClients.create(settings);
-        MongoDatabase database = mongoClient.getDatabase("testDB");
-        MongoCollection<Document> collection = database.getCollection("testCol");
-        BsonDocument filter = new BsonDocument();
-        collection.countDocuments(filter);
-
-        mongoClient.close();
-
         applicationContext =
                 new AnnotationConfigApplicationContext(ClosetApiApplication.class);
 
