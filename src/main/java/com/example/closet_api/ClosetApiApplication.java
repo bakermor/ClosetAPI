7cc60fa9fb31e7c097f81e1bf295b5a29154dfcf
Helen Schawe <helen.schawe@gmail.com>
2025-03-29 13:13:20 -0500

    MongoDB almost working, no import issues

--- src/main/java/com/example/closet_api/ClosetApiApplication.java
+++ src/main/java/com/example/closet_api/ClosetApiApplication.java
@@ -1,6 +1,11 @@
 package com.example.closet_api;
 
+// Import Java modules
+import java.lang.System;
+
 // Import external dependencies (Spring)
+import org.bson.BsonDocument;
+import org.bson.Document;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
@@ -9,6 +14,34 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.FindOneAndUpdateOptions;
+import com.mongodb.client.model.ReturnDocument;
+import com.mongodb.client.model.Updates;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.InsertManyResult;
+import org.bson.codecs.configuration.CodecRegistry;
+//import org.bson.codecs.pojo.PojoCodecProvider;
+import org.bson.conversions.Bson;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
 
 // Import our storage service
 import com.example.closet_api.storage.StorageProperties;
@@ -21,17 +54,6 @@ public class ClosetApiApplication {
 
     private static ApplicationContext applicationContext;
 
-	public static void main(String[] args) {
-        applicationContext =
-                new AnnotationConfigApplicationContext(ClosetApiApplication.class);
-
-        for (String beanName : applicationContext.getBeanDefinitionNames()) {
-            System.out.println(beanName);
-        }
-
-		SpringApplication.run(ClosetApiApplication.class, args);
-	}
-
     @Bean
     CommandLineRunner init(StorageService storageService) {
         /* delete and re-create storage folder at startup */
@@ -39,5 +61,36 @@ public class ClosetApiApplication {
             storageService.deleteAll();
             storageService.init();
         };
+    }
+
+    public static void main(String[] args) {
+        // Set system properties via commandline or programmatically
+        System.setProperty("javax.net.ssl.keyStore", "./cert/closet-api-app-1_x509.pem");
+        //System.setProperty("javax.net.ssl.keyStorePassword", "<keystore_password>");
+
+        String uri = "mongodb+srv://closetdb.o2kklsl.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&appName=ClosetDB";
+        ConnectionString connectionString = new ConnectionString(uri);
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .serverApi(ServerApi.builder()
+                        .version(ServerApiVersion.V1)
+                        .build())
+                .build();
+        MongoClient mongoClient = MongoClients.create(settings);
+        MongoDatabase database = mongoClient.getDatabase("testDB");
+        MongoCollection<Document> collection = database.getCollection("testCol");
+        BsonDocument filter = new BsonDocument();
+        collection.countDocuments(filter);
+
+        mongoClient.close();
+
+        applicationContext =
+                new AnnotationConfigApplicationContext(ClosetApiApplication.class);
+
+        for (String beanName : applicationContext.getBeanDefinitionNames()) {
+            System.out.println(beanName);
+        }
+
+        SpringApplication.run(ClosetApiApplication.class, args);
     }
 }
